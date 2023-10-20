a1567fd94b81f7e870503b834ded25bf880d3d52
Walid Said <wsaidamin@gmail.com>
2023-10-20 19:34:48 +0300

    Add axios package

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
 
 const app = createApp(App)
 
 app.use(router)
+
+axios.defaults.baseURL = 'http://localhost:8000/api';
 
 app.mount('#app')
