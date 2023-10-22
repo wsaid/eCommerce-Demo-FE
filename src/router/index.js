c00efc31297fdf177e80a8fee0531fe339e3c774
Walid Said <wsaidamin@gmail.com>
2023-10-22 03:37:34 +0300

    Add page routers

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,26 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('../views/CartView.vue')
+    },
+    {
+      path: '/payment',
+      name: 'payment',
+      component: () => import('../views/PaymentView.vue')
+    },
+    {
+      path: '/payment-success',
+      name: 'payment-success',
+      component: () => import('../views/PaymentSuccessView.vue')
+    },
+    {
+      path: '/payment-fail',
+      name: 'payment-fail',
+      component: () => import('../views/PaymentFailView.vue')
     }
   ]
 })
